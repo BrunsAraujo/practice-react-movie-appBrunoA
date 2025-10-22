c3f0a5541f99d77bd3a776d324e08ac377d19299
Bruno Araujogit <bruno.correadearaujo@gmail.com>
2025-10-22 14:22:03 -0500

    In class Paid work finalized at home

---
+++ React-Movie-App/src/App.jsx
@@ -0,0 +1,13 @@
+import MovieSelector from './Components/MovieSelector.jsx'
+
+function App() {
+        return (
+            <div className= "App">            
+            <p> MOVIE SELECTOR</p>    
+            <MovieSelector/>
+            </div>
+        )
+
+}
+
+export default App
